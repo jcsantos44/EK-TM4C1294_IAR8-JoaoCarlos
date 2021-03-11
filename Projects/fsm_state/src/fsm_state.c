2d92849f0de4216af7fc119823c2b00e7bebc886
jcsantos44 <jc.santos4@hotmail.com>
2021-03-11 11:37:44 -0300

    lab03 pronto - modificado fsm_event

--- Projects/fsm_state/src/fsm_state.c
+++ Projects/fsm_state/src/fsm_state.c
@@ -7,7 +7,7 @@
 // MEF com apenas 2 estados e 1 evento temporal que alterna entre eles
 // Seleção por estado
 
-typedef enum {Desligado, Ligado} state_t;
+//typedef enum {Desligado, Ligado} state_t;
 
 volatile uint8_t Evento = 0;
 
@@ -16,29 +16,58 @@ void SysTick_Handler(void){
 } // SysTick_Handler
 
 void main(void){
-  static state_t Estado = Desligado; // estado inicial da MEF
+  static int Estado = 0; // estado inicial da MEF
   
   LEDInit(LED1);
+  LEDInit(LED2);
+  LEDInit(LED3);
   SysTickPeriodSet(12000000); // f = 1Hz para clock = 24MHz
   SysTickIntEnable();
   SysTickEnable();
 
   while(1){
-    switch(Estado){
-    case Desligado:
-      LEDOff(LED1);
-      if(Evento){
-        Evento = 0;
-        Estado = Ligado;
-      } // if
-      break;
-    case Ligado:
-      LEDOn(LED1);
-      if(Evento){
-        Evento = 0;
-        Estado = Desligado;
-      } // if
-      break;
-    } // switch
+    if(Evento){
+      Evento=0;
+      switch(Estado){
+      case 0:
+        LEDOff(LED1);
+        LEDOff(LED2);
+        LEDOff(LED3);
+        Estado = 1;
+        break;
+      case 1:
+        LEDOff(LED1);
+        LEDOff(LED2);
+        LEDOn(LED3);
+        Estado = 2;
+        break;
+      case 2:
+        LEDOff(LED1);
+        LEDOn(LED2);
+        LEDOn(LED3);
+        Estado = 3;
+        break;
+      case 3:
+        LEDOff(LED1);
+        LEDOn(LED2);
+        LEDOff(LED3);
+        Estado = 4;
+        break;
+      case 4:
+        LEDOn(LED1);
+        LEDOn(LED2);
+        LEDOff(LED3);
+        Estado = 5;
+        break;
+      case 5:
+        LEDOn(LED1);
+        LEDOn(LED2);
+        LEDOn(LED3);
+        Estado = 0;
+        break;
+      }
+    }
+    
+    // switch
   } // while
 } // main
